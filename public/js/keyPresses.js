f15474009cf2ecdbca6213bd466c44157a0986f1
Aaron Cordova <theqodesmith@gmail.com>
2015-04-04 16:04:01 -0400

    little work, no big deal.

--- public/js/keyPresses.js
+++ public/js/keyPresses.js
@@ -17,63 +17,74 @@ var keyPresses = function() {
 		//////////////
 
 		// 1-4
-		if(e.which == 49) {
+		if(e.which === 49) {
 			console.log('1 pressed');
 			padTriggerTop(1);
-		} else if (e.which == 50) {
+		} else if (e.which === 50) {
 			console.log('2 pressed');
 			padTriggerTop(2);
-		} else if (e.which == 51) {
+		} else if (e.which === 51) {
 			console.log('3 pressed');
 			padTriggerTop(3);
-		} else if (e.which == 52) {
+		} else if (e.which === 52) {
 			console.log('4 pressed');
 			padTriggerTop(4);
 		} 
 
 		// q,w,e,r
-		else if (e.keyCode == 81) {
+		else if (e.keyCode === 81) {
 			console.log('q pressed');
 			padTriggerTop(5);
-		} else if (e.which == 87) {
+		} else if (e.which === 87) {
 			console.log('w pressed');
 			padTriggerTop(6);
-		} else if (e.which == 69) {
+		} else if (e.which === 69) {
 			console.log('e pressed');
 			padTriggerTop(7);
-		} else if (e.which == 82) {
+		} else if (e.which === 82) {
 			console.log('r pressed');
 			padTriggerTop(8);
 		} 
 
 		// a,s,d,f
-		else if (e.which == 65) {
+		else if (e.which === 65) {
 			console.log('a pressed');
 			padTriggerBottom(1);
-		} else if (e.which == 83) {
+		} else if (e.which === 83) {
 			console.log('s pressed');
 			padTriggerBottom(2);
-		} else if (e.which == 68) {
+		} else if (e.which === 68) {
 			console.log('d pressed');
 			padTriggerBottom(3);
-		} else if (e.which == 70) {
+		} else if (e.which === 70) {
 			console.log('f pressed');
 			padTriggerBottom(4);
 		} 
 
 		// z,x,c,v
-		else if (e.which == 90) {
+		else if (e.which === 90) {
 			console.log('z pressed');
 			padTriggerBottom(5);
-		} else if (e.which == 88) {
+		} else if (e.which === 88) {
 			console.log('x pressed');
 			padTriggerBottom(6);
-		} else if (e.which == 67) {
+		} else if (e.which === 67) {
 			console.log('c pressed');
 			padTriggerBottom(7);
-		} else if (e.which == 86) {
+		} else if (e.which === 86) {
 			console.log('v pressed');
 			padTriggerBottom(8);
+		}
+
+		// space bar - controls play button
+		else if (e.which === 32) {
+			console.log('space bar pressed');
+			controls.play();
+
+		// enter - controls record button
+		} else if (e.which === 13) {
+			console.log('enter pressed');
+			controls.record();
 		};
 	});
 
