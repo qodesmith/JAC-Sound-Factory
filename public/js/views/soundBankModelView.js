7116394e18cf21f6c165f6572f48426271f00b79
Aaron Cordova <theqodesmith@gmail.com>
2015-04-05 17:52:42 -0400

    Fixed bug of disappearing sound banks.

--- public/js/views/soundBankModelView.js
+++ public/js/views/soundBankModelView.js
@@ -16,25 +16,26 @@ App.Views.SoundBankModelView = Backbone.View.extend({
 		var collection = this.model.attributes.id;
 		var collectionType = this.model.attributes.type;
 		var collectionName = this.model.attributes.name;
-		
-		if(collectionType === 'fx') {
-			// Populate the side container to show
-			// which sound back is the active one.
-			$('#side-top').html(collectionName);
 
+		if(collectionType === 'fx') {
 			// Empty the view containing the collection.
 			$('#top8').empty();
 
 			// Set our current collection's url to the new id of our clicked bank.
 			App.soundsFXCollection.url = 'sound_banks/' + collection + '/sounds';
+			
+			// Reset is necessary to empty the collection first.
+			// If we don't and we click the same drum kit on the
+			// screen twice, it will disappear. Reseting 1st prevents that.
+			App.soundsFXCollection.reset();
 
 			// Fetch: will replace the old collection with the new one via the url.
 			App.soundsFXCollection.fetch();
 
 		} else if(collectionType === 'drums') {
-			$('#side-bot').html(collectionName);
 			$('#bot8').empty();
 			App.soundsDrumsCollection.url = 'sound_banks/' + collection + '/sounds';
+			App.soundsDrumsCollection.reset();
 			App.soundsDrumsCollection.fetch();
 		};
 	},
